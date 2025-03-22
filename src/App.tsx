1488f06958250abdc0ba8d7c01d7de7fb4d715db
qhsang <105709672+qhsang@users.noreply.github.com>
2025-03-22 18:51:28 +0700

    add unity webgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import './App.css'
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+    const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+        loaderUrl: "/testgit/build/webgl.loader.js",
+        dataUrl: "/testgit/build/webgl.data",
+        frameworkUrl: "/testgit/build/webgl.framework.js",
+        codeUrl: "/testgit/build/webgl.wasm",
+    });
 
-  return (
-      <>
-          <h1>Test Git</h1>
-          <h2>Test Git</h2>
-      </>
-  )
+    // We'll round the loading progression to a whole number to represent the
+    // percentage of the Unity Application that has loaded.
+    const loadingPercentage = Math.round(loadingProgression * 100);
+
+    return (
+        <div className="container">
+            {isLoaded === false && (
+                // We'll conditionally render the loading overlay if the Unity
+                // Application is not loaded.
+                <div className="loading-overlay">
+                    <p>Loading... ({loadingPercentage}%)</p>
+                </div>
+            )}
+            <Unity className="unity" unityProvider={unityProvider} />
+        </div>
+    );
 }
 
-export default App
+export default App;
